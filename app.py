a30523208c9c8aefeb318743f0b6e4958f7c48da
noahlee1234 <109221464+noahlee1234@users.noreply.github.com>
2026-04-10 22:16:37 -0700

    Restore demo.launch() call required for Docker SDK

--- app.py
+++ app.py
@@ -14,3 +14,9 @@ module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
 demo = module.build_ui()
+
+if __name__ == '__main__':
+    demo.launch(
+        server_name='0.0.0.0',
+        server_port=7860,
+    )
